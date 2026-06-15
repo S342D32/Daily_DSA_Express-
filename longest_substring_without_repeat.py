5c648b3cc1ee063d06dec87d07b12a5a1d8c49f2
s342d32 <sourajitnayak3471@gmail.com>
2026-06-15 23:44:37 +0530

    longest_substring_without_repeat:O(N)

--- longest_substring_without_repeat.py
+++ longest_substring_without_repeat.py
@@ -1,20 +1,43 @@
+# def longest_substring(s):
+#   n=len(s)
+#   max_len =0
+#   for i in range(n):
+#     seen=[]
+#     for j in range(i,n):
+
+#       if s[j] in seen:
+       
+#           break
+#       seen.append(s[j])
+#     max_len = max(max_len,len(seen))
+#   return max_len
+
+
+
+
+# s="acbcdefgef"
+# print(longest_substring(s))
+
+# TC:O(N^2)
+# SC :O(N)
+
+
 def longest_substring(s):
-  t=[]
   n=len(s)
   max_len =0
-  for i in range(n):
-    seen=[]
-    for j in range(i,n):
-
-      if s[j] in seen:
-       
-          break
-      seen.append(s[j])
-    max_len = max(max_len,len(seen))
+  left =0
+  seen=set()
+  for right in range(n):
+    while s[right] in seen:
+      seen.remove(s[left])
+      left+=1
+    seen.add(s[right])
+    max_len = max(max_len,right-left+1)
   return max_len
-
-
 
 
 s="acbcdefgef"
 print(longest_substring(s))
+
+# TC:O(N)
+# SC :O(N)
