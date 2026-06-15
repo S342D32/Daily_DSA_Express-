fcbc2ef007c7f4c048fc6b0aac21080db94987b0
s342d32 <sourajitnayak3471@gmail.com>
2026-06-15 23:04:35 +0530

    longest_substring_without_repeat

---
+++ longest_substring_without_repeat.py
@@ -0,0 +1,20 @@
+def longest_substring(s):
+  t=[]
+  n=len(s)
+  max_len =0
+  for i in range(n):
+    seen=[]
+    for j in range(i,n):
+
+      if s[j] in seen:
+       
+          break
+      seen.append(s[j])
+    max_len = max(max_len,len(seen))
+  return max_len
+
+
+
+
+s="acbcdefgef"
+print(longest_substring(s))
